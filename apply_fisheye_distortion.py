273892bf06945ced48562581e6fb6940b484ea57
Shreeyak <ss.shrek7@gmail.com>
2020-08-08 03:15:10 +0530

    added readme, omegaconf to manage config file, modify config file parameters from command line

--- apply_fisheye_distortion.py
+++ apply_fisheye_distortion.py
@@ -3,13 +3,14 @@ from pathlib import Path
 
 import cv2
 import numpy as np
+from omegaconf import OmegaConf
 import scipy.interpolate
-import yaml
 
 CAMERA_INTR_FILE = 'camera_intrinsics.txt'
+CONFIG_FILE = 'config.yaml'
 
 
-def distort_image(img: np.ndarray, cam_intr: np.ndarray, dist_coeff: np.ndarray) -> np.ndarray:
+def distort_image(img: np.ndarray, cam_intr: np.ndarray, dist_coeff: np.ndarray, crop_output: bool=True) -> np.ndarray:
     """Apply fisheye distortion to an image
 
     Args:
@@ -18,6 +19,8 @@ def distort_image(img: np.ndarray, cam_intr: np.ndarray, dist_coeff: np.ndarray)
                             Shape: (3, 3)
         dist_coeff (numpy.ndarray): The fisheye distortion coefficients, for OpenCV fisheye module.
                             Shape: (1, 4)
+        crop_output (bool): Whether to crop the output distorted image into a rectangle. The 4 corners of the input
+                            image will be mapped to 4 corners of the distorted image for cropping.
 
     Returns:
         numpy.ndarray: The distorted image, same resolution as input image. Unmapped pixels will be black in color.
@@ -48,23 +51,24 @@ def distort_image(img: np.ndarray, cam_intr: np.ndarray, dist_coeff: np.ndarray)
     img_dist = np.dstack([interpolator(undistorted_px) for interpolator in interpolators])
     img_dist = img_dist.clip(0, 255).astype(np.uint8)
 
-    # Crop rectangle from resulting distorted image
-    # Get mapping from undistorted to distorted
-    distorted_px = cv2.convertPointsToHomogeneous(img_pts)  # Shape: (N, 1, 3)
-    cam_intr_inv = np.linalg.inv(cam_intr)
-    distorted_px = np.tensordot(distorted_px, cam_intr_inv, axes=(2, 1))  # To camera coordinates, Shape: (N, 1, 3)
-    distorted_px = cv2.convertPointsFromHomogeneous(distorted_px)  # Shape: (N, 1, 2)
-    distorted_px = cv2.fisheye.distortPoints(distorted_px, cam_intr, dist_coeff)  # shape: (N, 1, 2)
-    distorted_px = distorted_px.reshape((h, w, 2))
-    # Get the corners. Round values up/down accordingly to avoid invalid pixel selection.
-    top_left = np.ceil(distorted_px[0, 0, :]).astype(np.int)
-    bottom_right = np.floor(distorted_px[(h - 1), (w - 1), :]).astype(np.int)
-    img_dist = img_dist[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0], :]
+    if crop_output:
+        # Crop rectangle from resulting distorted image
+        # Get mapping from undistorted to distorted
+        distorted_px = cv2.convertPointsToHomogeneous(img_pts)  # Shape: (N, 1, 3)
+        cam_intr_inv = np.linalg.inv(cam_intr)
+        distorted_px = np.tensordot(distorted_px, cam_intr_inv, axes=(2, 1))  # To camera coordinates, Shape: (N, 1, 3)
+        distorted_px = cv2.convertPointsFromHomogeneous(distorted_px)  # Shape: (N, 1, 2)
+        distorted_px = cv2.fisheye.distortPoints(distorted_px, cam_intr, dist_coeff)  # shape: (N, 1, 2)
+        distorted_px = distorted_px.reshape((h, w, 2))
+        # Get the corners. Round values up/down accordingly to avoid invalid pixel selection.
+        top_left = np.ceil(distorted_px[0, 0, :]).astype(np.int)
+        bottom_right = np.floor(distorted_px[(h - 1), (w - 1), :]).astype(np.int)
+        img_dist = img_dist[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0], :]
 
     return img_dist
 
 
-def main(args):
+def main():
     """This script creates fisheye distortion in images, using the OpenCV 4.4 fisheye camera model
     Look at equations in detailed description at: https://docs.opencv.org/4.4.0/db/d58/group__calib3d__fisheye.html
 
@@ -72,46 +76,44 @@ def main(args):
     The dimensions of output of distortion is ~58% of input image and may not be of the exact same aspect ratio due
     to rounding of pixel co-ordinate. To counter that, we resize the output according to config file.
 
-    Args:
-        args: object from argparse
+    The parameters in config file can be modified from the command line.
     """
-    dir_images = args.dir_images
-    dir_output = args.dir_output
-    config_file = args.config_file
-    ext_images = args.ext_images
+    base_conf = OmegaConf.load(CONFIG_FILE)
+    cli_conf = OmegaConf.from_cli()
+    conf = OmegaConf.merge(base_conf, cli_conf)
 
-    if not dir_images.exists() or not dir_images.is_dir():
-        raise ValueError(f'Not a directory: {dir_images}')
-    if not config_file.is_file():
-        raise ValueError(f'Not a file: {config_file}')
+    dir_input = Path(conf.dir_input)
+    dir_output = Path(conf.dir_output)
+    input_file_ext = conf.input_file_ext
+    if not dir_input.exists() or not dir_input.is_dir():
+        raise ValueError(f'Not a directory: {dir_input}')
     if not dir_output.exists():
         dir_output.mkdir(parents=True)
 
-    image_filenames = sorted(list(dir_images.glob('*' + ext_images)))
+    image_filenames = sorted(list(dir_input.glob('*' + input_file_ext)))
     num_images = len(image_filenames)
     if num_images < 1:
-        raise ValueError(f'No images found in dir {dir_images}, matching extention {ext_images}')
+        raise ValueError(f'No images found in dir {dir_input} that match file extention {input_file_ext}')
     else:
         print(f'Found {num_images} images. Applying distortion')
 
-    camera_intr_file = dir_images / CAMERA_INTR_FILE
+    camera_intr_file = dir_input / CAMERA_INTR_FILE
     K = np.loadtxt(str(camera_intr_file))
     print(f'Loaded camera intrinsics: \n{K}')
 
-    with open(config_file) as fd:
-        config = yaml.load(fd, Loader=yaml.Loader)
-    dist = config['distortion_parameters']
-    D = np.array([dist['k1'], dist['k2'], dist['k3'], dist['k4']])
+    dist = conf.distortion_parameters
+    D = np.array([dist.k1, dist.k2, dist.k3, dist.k4])
     print(f'Loaded distortion coefficients: {D}')
 
-    resize_h = int(config['resize_output']['h'])
-    resize_w = int(config['resize_output']['w'])
+    crop_output = conf.dev.crop_output
+    resize_h = int(conf.resize_output.h)
+    resize_w = int(conf.resize_output.w)
 
     for f_img in image_filenames:
         img = cv2.imread(str(f_img))
 
-        dist_img = distort_image(img, K, D)
-        if resize_h > 0:
+        dist_img = distort_image(img, K, D, crop_output=crop_output)
+        if resize_h > 0 and resize_w > 0 and crop_output is True:
             dist_img = cv2.resize(dist_img, (resize_w, resize_h), cv2.INTER_CUBIC)
 
         out_filename = dir_output / f"{f_img.stem}.dist{f_img.suffix}"
@@ -123,13 +125,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Apply fisheye effect to all matching images in a directory')
-    parser.add_argument('-i', '--dir_images', type=Path, required=True,
-                        help='Directory containing the images to distort', metavar='path/to/dir')
-    parser.add_argument('-e', '--ext_images', default='.rgb.png', help='Filename extention of images to convert')
-    parser.add_argument('-o', '--dir_output', type=Path, required=True,
-                        help='Path to save output images', metavar='path/to/dir')
-    parser.add_argument('-c', '--config_file', type=Path, default='config.yaml',
-                        help='Path to config file with distortion params', metavar='path/to/config.yaml')
-    args = parser.parse_args()
-    main(args)
+    main()
