5f222d51c42783416d97c21de6568892e749c30b
Shreeyak <ss.shrek7@gmail.com>
2020-08-15 06:34:16 +0530

    updated docker scripts

--- docker_build.sh
+++ docker_build.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 docker build --tag fisheye:1.0 .
--- docker_run.sh
+++ docker_run.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 # Description of the Docker command-line options shown below:
-#--init: Specifying an init process ensures the usual responsibilities of an init system, such as reaping zombie processes, are performed inside the created container
-#--mount type=bind,source=$PWD,target=/data: Mounts the current direct into /data in container.
-#--name cloud: Gives a name to this container for easy reference
+# --mount type=bind,source=$PWD,target=/data: Mounts the current directory at /data in container.
+# --name: Gives a name to this container for easy reference.
+# --workdir: Sets the working directory inside the container.
 
 docker run --rm -it --init \
   --mount type=bind,src=$PWD/images,dst=/data \
   --name fisheye \
-  -w /opt/fisheye-distortion \
+  --workdir /opt/fisheye-distortion \
   fisheye:1.0 /bin/bash
